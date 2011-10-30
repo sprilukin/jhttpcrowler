5dd8ce2ad7f2ec0ce2c2aa1a8539bfae9b902f7e
Sergey Prilukin <sprilukin@gmail.com>
2011-10-30 20:38:03 +0200

    Added cache to properties util

--- src/main/java/jhttpcrowler/utils/properties/PropertiesUtil.java
+++ src/main/java/jhttpcrowler/utils/properties/PropertiesUtil.java
@@ -5,6 +5,8 @@ import org.apache.commons.logging.LogFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -14,30 +16,39 @@ import java.util.Properties;
  */
 public class PropertiesUtil {
     private static final Log log = LogFactory.getLog(PropertiesUtil.class);
+    private static final Map<String, Properties> propsCache = new HashMap<String, Properties>();
 
     public static Properties load(String file, Properties defaultProps) {
-        Properties props = new Properties(defaultProps);
+        if (propsCache.get(file) == null) {
+            synchronized (PropertiesUtil.class) {
+                if (propsCache.get(file) == null) {
+                    Properties props = new Properties(defaultProps);
 
-        InputStream fileResource = Thread.currentThread().getContextClassLoader().getResourceAsStream(file);
-        if (fileResource == null) {
-            if (log.isWarnEnabled()) {
-                log.warn(String.format("[%s] doesn't exists in classpath", file));
+                    InputStream fileResource = Thread.currentThread().getContextClassLoader().getResourceAsStream(file);
+                    if (fileResource == null) {
+                        if (log.isWarnEnabled()) {
+                            log.warn(String.format("[%s] doesn't exists in classpath", file));
+                        }
+                        return null;
+                    }
+
+                    try {
+                        props.load(fileResource);
+                        if (log.isDebugEnabled()) {
+                            log.debug(String.format("[%s] loaded successfully", file));
+                        }
+                    } catch (IOException e) {
+                        if (log.isWarnEnabled()) {
+                            log.warn(String.format("error loading properties file: [%s]", file));
+                        }
+                        return null;
+                    }
+
+                    propsCache.put(file, props);
+                }
             }
-            return null;
         }
 
-        try {
-            props.load(fileResource);
-            if (log.isDebugEnabled()) {
-                log.debug(String.format("[%s] loaded successfully", file));
-            }
-        } catch (IOException e) {
-            if (log.isWarnEnabled()) {
-                log.warn(String.format("error loading properties file: [%s]", file));
-            }
-            return null;
-        }
-
-        return props;
+        return propsCache.get(file);
     }
 }
